0eb98786eebf317a8b6ac6ed4ff380f65be2670e
newstore0077 <91786879+newstore0077@users.noreply.github.com>
2022-03-06 00:27:39 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -4,30 +4,6 @@ import random, string, urllib.request, json, getpass
 
 password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(20))
 
-#Download ngrok
-
-wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-
-! unzip -qq -n ngrok-stable-linux-amd64.zip
-
-#Setup sshd
-
-! apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
-
-#Set root password
-
-! echo root:$password | chpasswd
-
-! mkdir -p /var/run/sshd
-
-! echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-
-! echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-
-! echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
-
-! echo "export LD_LIBRARY_PATH" >> /root/.bashrc
-
 #Run sshd
 
 get_ipython().system_raw('/usr/sbin/sshd -D &')
